03c02e228f79e8c122d98f9697dbe58041a133d8
Jose Ignacio Galarza <jose@canva.com>
2020-03-30 12:12:51 +1100

    Better UX

--- reversi.py
+++ reversi.py
@@ -23,22 +23,23 @@ class Board():
         b = self.board
         return f"""    1  2  3  4  5  6  7  8
    {BACK_GREEN} ─────────────────────── {RESET_COLOR}
- A {BACK_GREEN}│{b[0][0]}│{b[0][1]}│{b[0][2]}│{b[0][3]}│{b[0][4]}│{b[0][5]}│{b[0][6]}│{b[0][7]}│{RESET_COLOR}
-   {BACK_GREEN}│──┼──┼──┼──┼──┼──┼──┼──│{RESET_COLOR}
- B {BACK_GREEN}│{b[1][0]}│{b[1][1]}│{b[1][2]}│{b[1][3]}│{b[1][4]}│{b[1][5]}│{b[1][6]}│{b[1][7]}│{RESET_COLOR}
-   {BACK_GREEN}│──┼──┼──┼──┼──┼──┼──┼──│{RESET_COLOR}
- C {BACK_GREEN}│{b[2][0]}│{b[2][1]}│{b[2][2]}│{b[2][3]}│{b[2][4]}│{b[2][5]}│{b[2][6]}│{b[2][7]}│{RESET_COLOR}
-   {BACK_GREEN}│──┼──┼──┼──┼──┼──┼──┼──│{RESET_COLOR}
- D {BACK_GREEN}│{b[3][0]}│{b[3][1]}│{b[3][2]}│{b[3][3]}│{b[3][4]}│{b[3][5]}│{b[3][6]}│{b[3][7]}│{RESET_COLOR}
-   {BACK_GREEN}│──┼──┼──┼──┼──┼──┼──┼──│{RESET_COLOR}
- E {BACK_GREEN}│{b[4][0]}│{b[4][1]}│{b[4][2]}│{b[4][3]}│{b[4][4]}│{b[4][5]}│{b[4][6]}│{b[4][7]}│{RESET_COLOR}
-   {BACK_GREEN}│──┼──┼──┼──┼──┼──┼──┼──│{RESET_COLOR}
- F {BACK_GREEN}│{b[5][0]}│{b[5][1]}│{b[5][2]}│{b[5][3]}│{b[5][4]}│{b[5][5]}│{b[5][6]}│{b[5][7]}│{RESET_COLOR}
-   {BACK_GREEN}│──┼──┼──┼──┼──┼──┼──┼──│{RESET_COLOR}
- G {BACK_GREEN}│{b[6][0]}│{b[6][1]}│{b[6][2]}│{b[6][3]}│{b[6][4]}│{b[6][5]}│{b[6][6]}│{b[6][7]}│{RESET_COLOR}
-   {BACK_GREEN}│──┼──┼──┼──┼──┼──┼──┼──│{RESET_COLOR}
- H {BACK_GREEN}│{b[7][0]}│{b[7][1]}│{b[7][2]}│{b[7][3]}│{b[7][4]}│{b[7][5]}│{b[7][6]}│{b[7][7]}│{RESET_COLOR}
-   {BACK_GREEN} ─────────────────────── {RESET_COLOR}"""
+ A {BACK_GREEN}│{b[0][0]}│{b[0][1]}│{b[0][2]}│{b[0][3]}│{b[0][4]}│{b[0][5]}│{b[0][6]}│{b[0][7]}│{RESET_COLOR} A
+   {BACK_GREEN}│──┼──┼──┼──┼──┼──┼──┼──│{RESET_COLOR}  
+ B {BACK_GREEN}│{b[1][0]}│{b[1][1]}│{b[1][2]}│{b[1][3]}│{b[1][4]}│{b[1][5]}│{b[1][6]}│{b[1][7]}│{RESET_COLOR} B
+   {BACK_GREEN}│──┼──┼──┼──┼──┼──┼──┼──│{RESET_COLOR}  
+ C {BACK_GREEN}│{b[2][0]}│{b[2][1]}│{b[2][2]}│{b[2][3]}│{b[2][4]}│{b[2][5]}│{b[2][6]}│{b[2][7]}│{RESET_COLOR} C
+   {BACK_GREEN}│──┼──┼──┼──┼──┼──┼──┼──│{RESET_COLOR}  
+ D {BACK_GREEN}│{b[3][0]}│{b[3][1]}│{b[3][2]}│{b[3][3]}│{b[3][4]}│{b[3][5]}│{b[3][6]}│{b[3][7]}│{RESET_COLOR} D
+   {BACK_GREEN}│──┼──┼──┼──┼──┼──┼──┼──│{RESET_COLOR}  
+ E {BACK_GREEN}│{b[4][0]}│{b[4][1]}│{b[4][2]}│{b[4][3]}│{b[4][4]}│{b[4][5]}│{b[4][6]}│{b[4][7]}│{RESET_COLOR} E
+   {BACK_GREEN}│──┼──┼──┼──┼──┼──┼──┼──│{RESET_COLOR}  
+ F {BACK_GREEN}│{b[5][0]}│{b[5][1]}│{b[5][2]}│{b[5][3]}│{b[5][4]}│{b[5][5]}│{b[5][6]}│{b[5][7]}│{RESET_COLOR} F
+   {BACK_GREEN}│──┼──┼──┼──┼──┼──┼──┼──│{RESET_COLOR}  
+ G {BACK_GREEN}│{b[6][0]}│{b[6][1]}│{b[6][2]}│{b[6][3]}│{b[6][4]}│{b[6][5]}│{b[6][6]}│{b[6][7]}│{RESET_COLOR} G
+   {BACK_GREEN}│──┼──┼──┼──┼──┼──┼──┼──│{RESET_COLOR}  
+ H {BACK_GREEN}│{b[7][0]}│{b[7][1]}│{b[7][2]}│{b[7][3]}│{b[7][4]}│{b[7][5]}│{b[7][6]}│{b[7][7]}│{RESET_COLOR} H
+   {BACK_GREEN} ─────────────────────── {RESET_COLOR}
+    1  2  3  4  5  6  7  8"""
 
     def has_dominated(self, player):
         """
@@ -103,7 +104,6 @@ class Board():
         return flips
 
     def _get_flips_in_direction(self, player, move, direction):
-        # print(f"getting flips in direction: {direction}")
         flips = []
         x, y = move
         xd, yd = direction
